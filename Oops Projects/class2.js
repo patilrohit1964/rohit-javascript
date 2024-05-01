466f3607c3a975387931d0336a78b74815259eb2
rp_blazzer <113540316+patilrohit1964@users.noreply.github.com>
2024-05-01 11:45:44 +0000

    update 3 class

---
+++ Oops Projects/class2.js
@@ -0,0 +1,24 @@
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+    getColor() {
+        return this.color;
+    }
+}
+
+class Circle extends Shape {
+    constructor(color, radius) {
+        super(color);
+        this.radius = radius;
+    }
+}
+
+class ColoredCircle extends Circle {
+    constructor(color, radius) {
+        super(color, radius);
+    }
+}
+
+let coloredCircle = new ColoredCircle('blue', 5);
+console.log(coloredCircle.getColor());
---
+++ Oops Projects/class3.js
@@ -0,0 +1,23 @@
+class Vehicle {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+    getInfo() {
+        return `Make: ${this.make}, Model: ${this.model}`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model);
+        this.year = year;
+    }
+    getInfo() {
+        return `${super.getInfo()}, Year: ${this.year}`;
+    }
+}
+
+let myCar = new Car('Toyota', 'Camry', 2020);
+console.log(myCar.getInfo());
+
